a480bdcb8ba66647964d2190504653217c66e96e
zollie <thaonguyen07112004@gmail.com>
2025-09-10 20:55:46 +0700

    add error lib

---
+++ BuidingBlocks.Common/ApiResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+public class ApiResponse()
+{
+    public bool Success { get; init; }
+    public string? Message { get; init; }
+    public T? Data { get; init; }
+
+    public static ApiResponse<T> Ok(T data, string? message = null)
+        => new() { Success = true, Data = data, Message = message };
+
+    public static ApiResponse<T> Fail(string message)
+        => new() { Success = false, Message = message };
+}
+
--- services/FitnessService/FitnessService.API/Program.cs
+++ services/FitnessService/FitnessService.API/Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
